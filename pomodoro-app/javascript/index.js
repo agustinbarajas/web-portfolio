fff06010f595643802a03c97d7420f8e17ed432f
Agustin Barajas <agustinbvaldivia@gmail.com>
2022-06-23 20:09:39 -0600

    Add event listener to report modal tabs

--- pomodoro-app/javascript/index.js
+++ pomodoro-app/javascript/index.js
@@ -6,13 +6,22 @@ const handleModal = evt => {
 	document.body.appendChild(reportModal.content.cloneNode(true));
 
 	const closeModal = document.querySelector('.modal-close');
+	const reportModalTabButton = document.querySelectorAll('.report-header__tab');
 
 	const handleCloseModal = evt => {
 		evt.preventDefault();
 		document.body.removeChild(document.body.lastElementChild)
 	};
 
+	const handleModalTab = evt => {
+		evt.preventDefault();
+		document.querySelector('.report-header__tab.tab__active')
+			.classList.remove('tab__active');
+		evt.currentTarget.classList.add('tab__active');
+	};
+
 	closeModal.addEventListener('click', handleCloseModal);
+	reportModalTabButton.forEach(element => element.addEventListener('click', handleModalTab));
 }
 
 reportButton.addEventListener('click', handleModal);
